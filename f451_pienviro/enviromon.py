71b3ee3af1cfd274d8ddfea3e4ce09fd398b81af
Martin Lanser <martinlanser@gmail.com>
2023-12-28 18:49:23 -0500

    bump version 1.0.1 -> 1.0.2

--- f451_pienviro/enviromon.py
+++ f451_pienviro/enviromon.py
@@ -70,7 +70,7 @@ install_rich_traceback(show_locals=True)
 # =========================================================
 #          G L O B A L S   A N D   H E L P E R S
 # =========================================================
-APP_VERSION = '1.0.1'
+APP_VERSION = '1.0.2'
 APP_NAME = 'f451 Labs - EnviroMon'
 APP_NAME_SHORT = 'EnviroMon'
 APP_LOG = 'f451-enviromon.log'      # Individual logs for devices with multiple apps
